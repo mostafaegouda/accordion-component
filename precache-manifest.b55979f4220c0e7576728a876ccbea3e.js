9f793280debaa62b67c666b4c3b0796915de38bb
Mostafa Gouda <mostafaegouda@gmail.com>
2022-05-23 16:46:00 +0200

    Updates

---
+++ precache-manifest.b55979f4220c0e7576728a876ccbea3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359f37f7b662c81805631e04f0a04e96",
+    "url": "/accordion-component/index.html"
+  },
+  {
+    "revision": "48f0a3b53fb1c503b2fe",
+    "url": "/accordion-component/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "11e2b97d487a12b7b136",
+    "url": "/accordion-component/static/js/2.f745c462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion-component/static/js/2.f745c462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f0a3b53fb1c503b2fe",
+    "url": "/accordion-component/static/js/main.a6be6ed4.chunk.js"
+  },
+  {
+    "revision": "0089367a7381316f8a73",
+    "url": "/accordion-component/static/js/runtime-main.1105d591.js"
+  }
+]);
